4d0d5db4481b3968277ad3b569681c75ed4d6140
Jusfr <jusfr.v@gmail.com>
2015-03-16 18:15:43 +0800

    添加分页及排序扩展方法

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -24,23 +24,43 @@ namespace Demo {
             //dbConfig.SetInterceptor(new SqlStatementInterceptor());
             var dbFactory = dbConfig.BuildSessionFactory();
 
-            CountingWithDispose(dbFactory);
+            //CountingWithDispose(dbFactory);
+            CRUD(dbFactory);
         }
 
-        private static void Crud() {
+        private static void CRUD(ISessionFactory dbFactory) {
             var entry = new Person {
-                Name = Guid.NewGuid().ToString(),
+                Name = Guid.NewGuid().ToString().Substring(0, 8),
                 Birth = DateTime.UtcNow.AddDays(Guid.NewGuid().GetHashCode() % 365),
-                Address = Guid.NewGuid().ToString(),
+                Address = Guid.NewGuid().ToString().Substring(0, 8),
                 Job = null,
             };
+
+            using (var dbContext = new NHibernateRepositoryContext(dbFactory)) {
+                var repository = new NHibernateRepository<Person>(dbContext);
+                Console.WriteLine("New Entry with name {0}", entry.Name);
+                repository.Create(entry);
+                Console.WriteLine("Entry created with id {0}", entry.Id);
+
+                entry.Name = Guid.NewGuid().ToString().Substring(0, 8);
+                repository.Update(entry);
+
+                entry = repository.Retrive(entry.Id);
+                Console.WriteLine("Entry updated with name {0}", entry.Name);
+
+                Console.WriteLine("Paging Entry");
+                var page = repository.All.OrderByDescending(r => r.Id).Paging(1, 10);
+                foreach (var item in page.Items) {
+                    Console.WriteLine("{0} {1}", item.Id, item.Name);
+                }
+            }
         }
 
-        private static void CountingNoDispose(ISessionFactory factory) {
+        private static void CountingNoDispose(ISessionFactory dbFactory) {
             Task.Factory.StartNew(() => {
                 new Task(() => {
                     for (int i = 0; i < 100; i++) {
-                        var dbContext = new NHibernateRepositoryContext(factory);
+                        var dbContext = new NHibernateRepositoryContext(dbFactory);
                         var repository = new NHibernateRepository<Person>(dbContext);
                         var query = repository as IQueryRepository<Person>;
                         //dbContext.Begin();
@@ -50,11 +70,11 @@ namespace Demo {
             }).Wait();
         }
 
-        private static void CountingWithDispose(ISessionFactory factory) {
+        private static void CountingWithDispose(ISessionFactory dbFactory) {
             Task.Factory.StartNew(() => {
                 new Task(() => {
                     for (int i = 0; i < 100; i++) {
-                        using (var dbContext = new NHibernateRepositoryContext(factory)) {
+                        using (var dbContext = new NHibernateRepositoryContext(dbFactory)) {
                             var repository = new NHibernateRepository<Person>(dbContext);
                             var query = repository as IQueryRepository<Person>;
                             //dbContext.Begin();
@@ -65,7 +85,7 @@ namespace Demo {
             }).Wait();
         }
 
-        private static void PrepareData(IRepository<Person> repository) {
+        private static void PrepareData(ISessionFactory factory) {
             var persons = Enumerable.Range(0, 60000)
                 .Select(i => new Person {
                     Name = Guid.NewGuid().ToString(),
@@ -73,9 +93,11 @@ namespace Demo {
                     Address = Guid.NewGuid().ToString(),
                     Job = null,
                 });
-
-            foreach (var person in persons) {
-                repository.Create(person);
+            using (var dbContext = new NHibernateRepositoryContext(factory)) {
+                var repository = new NHibernateRepository<Person>(dbContext);
+                foreach (var person in persons) {
+                    repository.Create(person);
+                }
             }
         }
     }
---
+++ src/Jusfr.Persistent/Paging.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jusfr.Persistent {
+    public class Paging {
+        public Int32 CurrentPage { get; set; }
+        public Int32 ItemsPerPage { get; set; }
+        public Int64 TotalItems { get; set; }
+        public Int32 TotalPages { get; set; }
+
+    }
+
+    public class Paging<T> : Paging {
+        public IList<T> Items { get; set; }
+
+        public Paging(IList<T> items, Int32 currentPage, Int32 itemsPerPage, Int64 totalItems) {
+            Items = items;
+            CurrentPage = currentPage;
+            ItemsPerPage = itemsPerPage;
+            TotalItems = totalItems;
+            TotalPages = (Int32)Math.Ceiling((Double)totalItems / itemsPerPage);
+        }
+
+        public Paging(IList<T> items, Paging pageBase) {
+            Items = items;
+            CurrentPage = pageBase.CurrentPage;
+            ItemsPerPage = pageBase.ItemsPerPage;
+            TotalItems = pageBase.TotalItems;
+            TotalPages = pageBase.TotalPages;
+        }
+    }
+}
