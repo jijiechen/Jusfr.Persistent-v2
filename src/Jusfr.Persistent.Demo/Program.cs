2c381a71adb64579428d3fe36aff7709428e8645
Jusfr <jusfr.w@gmail.com>
2015-09-10 13:09:01 +0800

    添加了关于 NonUniqueObjectException 及 evict 机制的测试逻辑

--- src/Jusfr.Persistent.Demo/Program.cs
+++ src/Jusfr.Persistent.Demo/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Linq;
 
 namespace Jusfr.Persistent.Demo {
@@ -12,7 +13,56 @@ namespace Jusfr.Persistent.Demo {
             Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
 
             //MongoBasicCrud();
-            NHibernateBasicCrud();
+            //NHibernateBasicCrud();
+            //Null_could_evict();
+            Dupliate_entity_update_need_evict();
+        }
+
+        private static void Dupliate_entity_update_need_evict() {
+            var context = new PubsContext();
+            var jobRepo = new NHibernateRepository<Job>(context);
+
+            var j1 = jobRepo.Retrive(1);
+            var j2 = jobRepo.Retrive(1);
+            Console.WriteLine("j1 == j2? {0}", j1 == j2); // True
+            
+            var j3 = new Job {
+                Id = j1.Id,
+                Salary = j2.Salary,
+                Title = j2.Title
+            };
+
+            try {
+                j3.Salary += 1;
+                jobRepo.Update(j3); //Failed
+                //a different object with the same identifier value was already associated with the session: 1, of entity: Jusfr.Persistent.Demo.Job
+                Debug.Fail("Should failed for NHibernate.NonUniqueObjectException");
+            }
+            catch (NHibernate.NonUniqueObjectException ex){
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Got NHibernate.NonUniqueObjectException with Entity {0}", ex.EntityName);
+                Console.ResetColor();
+            }
+
+            var session = context.EnsureSession();
+            Console.WriteLine("EntityCount before evict {0}", session.Statistics.EntityCount);
+            session.Evict(j1);  //移除j1的游离态，对j3的更新操作才能完成
+            Console.WriteLine("EntityCount after evict {0}", session.Statistics.EntityCount);
+
+            jobRepo.Update(j3); //Pass
+            Console.WriteLine("j1.Salary: {0}, j3.Salary: {1}", j1.Salary, j3.Salary);
+            // j1.Salary: 2760.00000, j3.Salary: 2761.00000
+        }
+
+        private static void Null_could_evict() {
+            var context = new PubsContext();
+            var jobRepo = new NHibernateRepository<Job>(context);
+
+            var session = context.EnsureSession();
+            session.Evict(null);
+
+            var jobNotExist = jobRepo.Retrive(-100);
+            session.Evict(jobNotExist);
         }
 
         private static void MongoBasicCrud() {
