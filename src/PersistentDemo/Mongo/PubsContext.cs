fca75b0f0d88f9f18e838fd1c4f63123f79c31b6
jusfrw <jusfr.v@gmail.com>
2016-06-23 09:53:19 +0800

    添加 demo 程序

---
+++ src/PersistentDemo/Mongo/PubsContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Jusfr.Persistent.Mongo;
+using MongoDB.Driver;
+
+namespace PersistentDemo.Mongo {
+    public class PubsContext : MongoRepositoryContext {
+        private static MongoUrl _mongoUrl;
+
+        static PubsContext() {
+            var url = ConfigurationManager.ConnectionStrings["PubsMongo"].ConnectionString;
+            _mongoUrl = new MongoUrl(url);
+        }
+
+        public PubsContext()
+            : base(_mongoUrl) {
+        }
+    }
+}
---
+++ src/PersistentDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistentDemo {
+    class Program {
+        static void Main(string[] args) {
+            Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            //MySql.Program.Retrive_via_primaryKey_medium_scale();
+            //MySql.Program.Save_entity_new_and_exists();
+            MySql.Program.Insert_with_dapper();
+            MySql.Program.Insert_with_nhibernate();
+
+            //Mongo.Program.Retrive_via_primaryKey_medium_scale();
+            //Mongo.Program.Save_entity_new_and_exists();
+        }
+    }
+}
