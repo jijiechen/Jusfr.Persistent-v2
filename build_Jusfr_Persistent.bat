1cbca2019b776d22fe0eb5a846e6d481d2dbfa11
Jijie Chen <jijie.chen@someplace.com>
2016-06-23 23:23:57 +0800

    更新了版本号；完善了编译脚本，以及生成 NuGet 包的 profile 文件

--- build_Jusfr_Persistent.bat
+++ build_Jusfr_Persistent.bat
@@ -1,22 +1,41 @@
 @setlocal 
 @set local=%~dp0
-@pushd %WINDIR%\Microsoft.NET\Framework\v4.0.30319\
+@ECHO off
+
+@REM Get path to MSBuild Binaries
+if exist "%ProgramFiles%\MSBuild\14.0\bin" SET MSBUILDEXEDIR=%ProgramFiles%\MSBuild\14.0\bin
+if exist "%ProgramFiles(x86)%\MSBuild\14.0\bin" SET MSBUILDEXEDIR=%ProgramFiles(x86)%\MSBuild\14.0\bin
+
+@REM Can't multi-block if statement when check condition contains '(' and ')' char, so do as single line checks
+if NOT "%MSBUILDEXEDIR%" == "" SET MSBUILDEXE=%MSBUILDEXEDIR%\MSBuild.exe
+if "%MSBUILDEXEDIR%" == "" GOTO :MsBuildNotFound
+if NOT "%MSBUILDEXEDIR%" == "" GOTO :MsBuildFound
+
+
+:MsBuildFound
+@ECHO MsBuild Location = %MSBUILDEXE%
 @goto build
 
 :build
-msbuild "%local%Jusfr.Persistent.sln" /t:Rebuild /P:Configuration=Release
+"%MSBUILDEXE%" "%local%Jusfr.Persistent.sln" /t:Rebuild /P:Configuration=Release
+@goto copy
+
+
+:copy
+robocopy "%local%src\Jusfr.Persistent.NET40\bin\Release" "%local%release" /e
+robocopy "%local%src\Jusfr.Persistent.NHibernate\bin\Release" "%local%release" /e
+robocopy "%local%src\Jusfr.Persistent.Mongo\bin\Release" "%local%release" /e
 @goto pack
 
 :pack
+%local%\.nuget\NuGet pack %local%\src\Jusfr.Persistent\Jusfr.Persistent.nuspec  -OutputDirectory %local%\release
 %local%\.nuget\NuGet pack %local%\src\Jusfr.Persistent.Mongo\Jusfr.Persistent.Mongo.nuspec  -OutputDirectory %local%\release
-@goto copy
-
-:copy
-robocopy "%local%src\Jusfr.Persistent\bin\Release" "%local%release" /e
-robocopy "%local%src\Jusfr.Persistent.NH\bin\Release" "%local%release" /e
-robocopy "%local%src\Jusfr.Persistent.Mongo\bin\Release" "%local%release" /e
 @goto end
+
 
 :end
 @pushd %local%
 @pause
+
+:MsBuildNotFound
+@ECHO Could not found msbuild
